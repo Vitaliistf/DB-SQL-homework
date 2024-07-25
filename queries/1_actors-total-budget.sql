57df7ea12e5dd286247c4f86b85382ce59ebb278
Vitalii Stefaniv <deosplayt@gmail.com>
2024-07-25 13:02:17 +0300

    feat: added first query

---
+++ queries/1_actors-total-budget.sql
@@ -0,0 +1,23 @@
+-- Вибрати список акторів разом із загальним бюджетом фільмів, в яких вони з’явилися
+-- ID
+-- First name
+-- Last name
+-- Total movies budget
+
+SELECT 
+	p.id as "ID", 
+	first_name as "First name", 
+	last_name as "Last name", 
+	SUM(m.budget) as "Total movies budget"
+FROM 
+	person p
+LEFT JOIN 
+	movie_person_character mpc ON p.id = mpc.person_id
+LEFT JOIN 
+	movie m ON mpc.movie_id = m.id
+GROUP BY 
+	p.id, p.first_name, p.last_name
+HAVING					-- Not including non-actors(directors)
+    SUM(m.budget) > 0
+ORDER BY 
+	"Total movies budget" DESC;
