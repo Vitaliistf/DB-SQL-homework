6d1af0bd2f31c522b1a04895b632eebc6524a235
Vitalii Stefaniv <deosplayt@gmail.com>
2024-07-25 10:35:50 +0300

    feat: added user table creation

--- create-database.sql
+++ create-database.sql
@@ -12,8 +12,22 @@ CREATE TABLE file (
 -- Table for countries
 CREATE TABLE country (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE,
+    name VARCHAR(100) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Table for users
+CREATE TABLE user (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    avatar_id INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+    FOREIGN KEY (avatar_id) REFERENCES file(id) ON DELETE SET NULL
+);
